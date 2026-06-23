bebdcb6de2f45212706d9f3b0844ef3212c04881
Piyush Jaiswal <piyush@Piyushs-MacBook-Air.local>
2026-06-23 19:30:03 +0530

    odels and controller

---
+++ src/main/java/com/example/productcatalogservice/models/Product.java
@@ -0,0 +1,13 @@
+package com.example.productcatalogservice.models;
+
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+public class Product extends BaseModel {
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+    private Category category;
+}
